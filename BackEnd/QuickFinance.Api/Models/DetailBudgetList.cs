9e7d754a05a9c0fb37d15895cba32767ab419561
Francis Mejia <7258418+FranMM7@users.noreply.github.com>
2024-10-16 09:08:05 -0600

    fix issue left behind on the backend

--- BackEnd/QuickFinance.Api/Models/DetailBudgetList.cs
+++ BackEnd/QuickFinance.Api/Models/DetailBudgetList.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 public class DetailBudgetList
 {
     public int Id { get; set; }
-    public decimal TotalBudget { get; set; }
+    public decimal TotalAllocatedBudget { get; set; }
     public decimal ExecutedBudget { get; set; }
     public string Title { get; set; }
     public DateTime ModifiedOn { get; set; }
